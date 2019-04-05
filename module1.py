f4d416f0b8e7b21cb6b0e922c37cf6e034c3ac10
esdee <mailedy28@gmail.com>
2019-04-05 01:22:32 +0100

    added

--- module1.py
+++ module1.py
@@ -1,6 +1,6 @@
 students=[
-    {"id":"001","name":"vick nick","age":32,"sex":"male","height":"167cm"},
-    {"id":"002","name":"bick nick","age":32,"sex":"male","height":"167cm"},
+    {"id":"sports","name":"vick nick","age":32,"sex":"male","height":"167cm"},
+    {"id":"action","name":"bick nick","age":32,"sex":"male","height":"167cm"},
     {"id":"003","name":"vick nick","age":32,"sex":"male","height":"167cm"},
     {"id":"004","name":"vick nick","age":32,"sex":"male","height":"167cm"},
     {"id":"005","name":"vick nick","age":32,"sex":"male","height":"167cm"}
@@ -10,5 +10,22 @@ def get_student_details (id,students):
     for student in students:
         if (student["id"]==id):
           return student
-student_info = get_student_details ("002",students)
+student_info = get_student_details ("sports",students)
 print(student_info)
+
+num = 1
+while num < 101:
+    if num % 15 == 0:
+        print('fizzbuzz')
+    elif num % 5 == 0:
+        print('buzz')
+    elif num % 3 == 0:
+        print('fizz')
+    else:
+        print(num)
+    num += 1  
+
+kg = 1000
+def convert_grammes_to_kilogrames (g):
+    return str(g/kg) + "kg"
+print (convert_grammes_to_kilogrames (5000)) 
